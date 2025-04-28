cd840bd4cbbe5fc819c5e59c6d1d5aa6bede44f0
Richard Okoro <glasshouse747@gmail.com>
2025-04-28 17:05:44 +0100

    ansible componentss

--- component/main.tf
+++ component/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_subnet_network_security_group_association" "existing_nsg" {
 # FRONTEND
 
 resource "azurerm_public_ip" "frontend" {
-  name                = "${var}.name-ip"
+  name                = "${var.name}-ip"
   resource_group_name = var.rg_name
   location            = var.location
   allocation_method   = "Static"
