925ab8b40b2f0a74b9ff51ce5f889a96ef8a9ae7
Richard Okoro <glasshouse747@gmail.com>
2025-04-30 15:41:01 +0100

    ansible componentss

--- terraform/misc-code/tf-state-init/main.tf
+++ terraform/misc-code/tf-state-init/main.tf
@@ -10,3 +10,10 @@ resource "azurerm_storage_account" "roboshop_tf_state" {
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
+
+resource "azurerm_storage_container" "roboshop_tf_state" {
+  name                  = "robsoshop-state-files"
+  storage_account_id    = azurerm_storage_account.roboshop_tf_state.id
+  container_access_type = "private"
+}
+
