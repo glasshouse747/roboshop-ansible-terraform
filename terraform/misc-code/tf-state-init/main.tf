b3c32195629f7d68f3da55a93d7d484bba187195
Richard Okoro <glasshouse747@gmail.com>
2025-04-30 15:26:57 +0100

    ansible componentss

---
+++ terraform/misc-code/tf-state-init/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "eb986b09-9743-4aa1-b10f-53da04d8708c"
+}
+
+resource "azurerm_storage_account" "roboshop_tf_state" {
+  name                     = "mydevopshop-tf-state"
+  resource_group_name      = "my-first-rg"
+  location                 = "UK West"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
