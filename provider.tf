144a9208125987c3205f70344ee6f467a92ece9a
Richard Okoro <glasshouse747@gmail.com>
2025-05-01 14:22:34 +0100

    terraform

--- provider.tf
+++ provider.tf
@@ -4,12 +4,5 @@ provider "azurerm" {
 }
 
 terraform {
-  backend "azurerm" {
-    use_cli              = true
-    subscription_id      = "eb986b09-9743-4aa1-b10f-53da04d8708c"
-    resource_group_name  = "my-first-rg"
-    storage_account_name = "mydevopshop"
-    container_name       = "roboshop-state-files"
-    # key                  = "test.terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
