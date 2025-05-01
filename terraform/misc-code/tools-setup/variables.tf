55052b573f0e337568c54b0872899a6accf970b2
Richard Okoro <glasshouse747@gmail.com>
2025-05-01 18:21:08 +0100

    terraform

--- terraform/misc-code/tools-setup/variables.tf
+++ terraform/misc-code/tools-setup/variables.tf
@@ -1,9 +1,12 @@
 variable "tools" {
-  vault {
-    vm_size = "Standard_DS1_v2"
-
+  default = {
+    vault = {
+      vm_size = "Standard_DS1_v2"
+    }
   }
 }
+
+
 
 variable "rg_name" {}
 variable "rg_location" {}
