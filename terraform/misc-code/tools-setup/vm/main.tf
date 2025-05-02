3f229a26e6aec9e5103ffa13d90bcc32d6f3b53e
Richard Okoro <glasshouse747@gmail.com>
2025-05-02 13:24:15 +0100

    terraform

--- terraform/misc-code/tools-setup/vm/main.tf
+++ terraform/misc-code/tools-setup/vm/main.tf
@@ -1,3 +1,25 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = var.name
+  location              = var.rg_location
+  resource_group_name   = var.rg_name
+  network_interface_ids = [azurerm_network_interface.private_ip.id]
+  size                  = var.vm_size
+  admin_username        = "azuser"
+  admin_password        = "Giveme123456"
+  disable_password_authentication = false
+  priority              = "Spot"
+  max_bid_price         = -1
+  eviction_policy       = "Deallocate"
+  source_image_id       = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Compute/images/local-devops-practice"
+
+  os_disk {
+    name                 = "${var.name}-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+}
+
 resource "azurerm_public_ip" "public_ip" {
   depends_on          = [azurerm_linux_virtual_machine.vm]
   name                = "${var.name}-ip"
@@ -44,24 +66,4 @@ resource "azurerm_dns_a_record" "private_dns_record" {
   records = [azurerm_network_interface.private_ip.private_ip_address]
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = var.name
-  location              = var.rg_location
-  resource_group_name   = var.rg_name
-  network_interface_ids = [azurerm_network_interface.private_ip.id]
-  size                  = var.vm_size
-  admin_username        = "azuser"
-  admin_password        = "Giveme123456"
-  disable_password_authentication = false
-  priority              = "Spot"
-  max_bid_price         = -1
-  eviction_policy       = "Deallocate"
-  source_image_id       = "/subscriptions/eb986b09-9743-4aa1-b10f-53da04d8708c/resourceGroups/my-first-rg/providers/Microsoft.Compute/images/local-devops-practice"
 
-  os_disk {
-    name                 = "${var.name}-disk"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-}
