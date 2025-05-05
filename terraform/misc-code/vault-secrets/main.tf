1a9535ddce68b54cb5c38c44785e9a2624efe086
Richard Okoro <glasshouse747@gmail.com>
2025-05-05 13:06:54 +0100

    roboshop x

--- terraform/misc-code/vault-secrets/main.tf
+++ terraform/misc-code/vault-secrets/main.tf
@@ -1,21 +1,8 @@
-variable "token" {}
-provider "vault" {
-  address = "http://vault.mydevops.shop:8200"
-  token = var.token
-}
-
 resource "vault_mount" "kv" {
   for_each    = var.secrets
   path        = each.key
   type        = "kv"
   options     = { version = "2" }
-  description = "KV Version 2 secret engine mount"
+  description = each.value["description"]
 }
 
-variable "secrets" {
-  default = {
-    roboshop-dev = {
-      description = "RoboShop App Component All secrets"
-    }
-  }
-}
---
+++ terraform/misc-code/vault-secrets/state.tf
@@ -0,0 +1,15 @@
+provider "vault" {
+  address = "http://vault.mydevops.shop:8200"
+  token = var.token
+}
+
+terraform {
+  backend "azurerm" {
+    subscription_id      = "eb986b09-9743-4aa1-b10f-53da04d8708c"
+    resource_group_name  = "my-first-rg"
+    storage_account_name = "mydevopshop"
+    container_name       = "roboshop-state-files"
+    key                  = "vault.terraform.tfstate"
+  }
+}
+
