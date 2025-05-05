54fea2afb01f486aa45959fcadb1f345575466a2
Richard Okoro <glasshouse747@gmail.com>
2025-05-05 16:38:11 +0100

    roboshop x

--- terraform/misc-code/vault-secrets/variables.tf
+++ terraform/misc-code/vault-secrets/variables.tf
@@ -16,20 +16,100 @@ variable "values" {
     cart ={
       secret = "roboshop-dev"
       value  = {
-        REDIS_HOST="redis-dev.mydevops.shop"
-        CATALOGUE_HOST="catalogue-dev.mydevops.shop"
-        CATALOGUE_PORT=8080
+        REDIS_HOST      = "redis-dev.mydevops.shop"
+        CATALOGUE_HOST  = "catalogue-dev.mydevops.shop"
+        CATALOGUE_PORT  = "8080"
         }
       }
+
+    catalogue = {
+      secret = "roboshop-dev"
+      value  = {
+        MONGO     = true
+        MONGO_URL = "mongodb://mongodb-dev.mydevops.shop:27017/catalogue"
+
+      }
+    }
+
+    dispatch = {
+      secret = "roboshop-dev"
+      value  = {
+        MONGO     = true
+        MONGO_URL = "mongodb://mongodb-dev.mydevops.shop:27017/catalogue"
+
+      }
+    }
 
     frontend = {
       secret = "roboshop-dev"
       value = {
-        catalogue_url = "http://catalogue-dev.mydevops.shop:8080/"
-        user_url = "http://user-dev.mydevops.shop:8080/"
-        cart_url = "http://cart-dev.mydevops.shop:8080/"
-        shipping_url = "http://shipping-dev.mydevops.shop:8080/"
-        payment_url = "http://payment-dev.mydevops.shop:8080/"
+        catalogue_url   = "http://catalogue-dev.mydevops.shop:8080/"
+        user_url        = "http://user-dev.mydevops.shop:8080/"
+        cart_url        = "http://cart-dev.mydevops.shop:8080/"
+        shipping_url    = "http://shipping-dev.mydevops.shop:8080/"
+        payment_url     = "http://payment-dev.mydevops.shop:8080/"
+      }
+    }
+
+    mongodb = {
+      secret = "roboshop-dev"
+      value  = {
+        MONGO     = true
+        MONGO_URL = "mongodb://mongodb-dev.mydevops.shop:27017/catalogue"
+
+      }
+    }
+
+    mysql = {
+      secret = "roboshop-dev"
+      value  = {
+        username  = "root"
+        password  = "RoboShop@1"
+
+
+      }
+    }
+
+    payment = {
+      secret = "roboshop-dev"
+      value  = {
+        CART_HOST   = "cart-dev.mydevops.shop"
+        CART_PORT   = "8080"
+        USER_HOST   = "user-dev.mydevops.shop"
+        USER_PORT   = "8080"
+        AMQP_HOST   = "rabbitmq-dev.mydevops.shop"
+        AMQP_USER   = "roboshop"
+        AMQP_PASS   = "roboshop123"
+      }
+    }
+
+    rabbitmq = {
+      secret = "roboshop-dev"
+      value  = {
+        username  = "roboshop"
+        password  = "roboshop123"
+
+
+      }
+    }
+
+    shipping = {
+      secret = "roboshop-dev"
+      value  = {
+       CART_ENDPOINT  = "cart-dev.mydevops.shop:8080"
+       DB_HOST        = "mysql-dev.mydevops.shop"
+       username       = "root"
+       password       = "RoboShop@1"
+
+      }
+    }
+
+    user = {
+      secret = "roboshop-dev"
+      value  = {
+        MONGO       =true
+        REDIS_URL   ="redis://redis-dev.mydevops.shop:6379"
+        MONGO_URL   ="mongodb://mongodb-dev.mydevops.shop:27017/users"
       }
     }
 
