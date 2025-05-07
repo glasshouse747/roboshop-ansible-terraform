0da9bc7c7ffbb6c36bde5dd6636dd48f5db3dd28
Richard Okoro <glasshouse747@gmail.com>
2025-05-07 12:44:09 +0100

    roboshop x

--- terraform/misc-code/tools-setup/provider.tf
+++ terraform/misc-code/tools-setup/provider.tf
@@ -5,11 +5,5 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    use_cli              = true
-    subscription_id      = "eb986b09-9743-4aa1-b10f-53da04d8708c"
-    resource_group_name  = "my-first-rg"
-    storage_account_name = "mydevopshop"
-    container_name       = "roboshop-state-files"
-    key                  = "tools.terraform.tfstate"
   }
 }
